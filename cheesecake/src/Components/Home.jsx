2e452b4b4c9dde04fa9b5cb96e11de59e501d59a
Abudu Rodiyyah <rodiyahabudu@gmail.com>
2024-04-19 23:20:55 +0100

    update

--- cheesecake/src/Components/Home.jsx
+++ cheesecake/src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import logo from '../images/LOGO OF CHEESECAKE.png';
@@ -10,27 +10,69 @@ import About from './About';
 import Menu from './Menu';
 
 function Home() {
+
+  const [activeLink, setActiveLink] = useState("Home");
+  const handleSetActiveLink = (link) => {
+    setActiveLink(link); // Update active link state when a link is clicked
+  };
+
   return (
     <section className='home' id='Home'>
-      <nav class="navbar navbar-expand-lg myNav">
-        <div class="container-fluid myContainer">
-          <img src={logo} />
-          <button class="navbar-toggler myToggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg myNav">
+        <div className="container-fluid myContainer">
+          <img src={logo} alt="Cheesecake Logo" />
+          <button className="navbar-toggler myToggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse lists" id="navbarNav">
-            <ul class="navbar-nav items">
-              <li class="nav-item actives">
-              <ScrollLink to="/" smooth={true} duration={500} class="nav-link" aria-current="page">Home</ScrollLink>
+          <div className="collapse navbar-collapse lists" id="navbarNav">
+            <ul className="navbar-nav items">
+              <li className={`nav-item ${activeLink === 'Home' ? 'actives' : ''}`}>
+                <ScrollLink
+                  to="Home"
+                  smooth={true}
+                  duration={500}
+                  className="nav-link"
+                  aria-current="page"
+                  onClick={() => handleSetActiveLink('Home')}
+                >
+                  Home
+                </ScrollLink>
               </li>
-              <li class="nav-item">
-              <ScrollLink to="about" smooth={true} duration={500} class="nav-link" aria-current="page">About Us</ScrollLink>
+              <li className={`nav-item ${activeLink === 'About' ? 'actives' : ''}`}>
+                <ScrollLink
+                  to="About"
+                  smooth={true}
+                  duration={500}
+                  className="nav-link"
+                  aria-current="page"
+                  onClick={() => handleSetActiveLink('About')}
+                >
+                  About Us
+                </ScrollLink>
               </li>
-              <li class="nav-item">
-              <ScrollLink to="menu" smooth={true} duration={500} class="nav-link" aria-current="page">Menu</ScrollLink>
+              <li className={`nav-item ${activeLink === 'Menu' ? 'actives' : ''}`}>
+                <ScrollLink
+                  to="Menu"
+                  smooth={true}
+                  duration={500}
+                  className="nav-link"
+                  aria-current="page"
+                  onClick={() => handleSetActiveLink('Menu')}
+                >
+                  Menu
+                </ScrollLink>
               </li>
-              <li class="nav-item">
-                <ScrollLink to="contact" smooth={true} duration={500} class="nav-link" aria-current="page">Contact</ScrollLink>
+              <li className={`nav-item ${activeLink === 'Contact' ? 'actives' : ''}`}>
+                <ScrollLink
+                  to="Contact"
+                  smooth={true}
+                  duration={500}
+                  className="nav-link"
+                  aria-current="page"
+                  onClick={() => handleSetActiveLink('Contact')}
+                >
+                  Contact
+                </ScrollLink>
               </li>
             </ul>
           </div>
